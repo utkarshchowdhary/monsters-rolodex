2dbbbb1eafb58f5eac6229e274c19090025afa1c
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-10-09 00:15:08 +0530

    Updates

---
+++ precache-manifest.fced063055a972ee3a7e0b2ff43818ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b64fa8d68e2d47cd99d73c504259a6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "76b7d5cbe541070eae63",
+    "url": "/monsters-rolodex/static/css/main.d87d2343.chunk.css"
+  },
+  {
+    "revision": "386f11a5ba599a084107",
+    "url": "/monsters-rolodex/static/js/2.9594307a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9594307a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b7d5cbe541070eae63",
+    "url": "/monsters-rolodex/static/js/main.a8e75edf.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
